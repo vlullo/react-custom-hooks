a87855efe5d850c59774af26aa0def36e9aefa1d
Valentina Lullo <valentinalullo88@gmail.com>
2021-01-25 23:39:26 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import usePosts from './usePosts'; 
+
+function App() {
+  const posts = usePosts();
+
+  const renderedPosts = posts.map(post => {
+    return <li key={post.id}>{post.title}</li>;
+  });
+  return (
+    <div>
+      <h3>Posts</h3>
+      <ul>{renderedPosts}</ul>
+    </div>
+  );
+}
+
+export default App;
